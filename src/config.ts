4e6191b60b9431a8c91302b444829ca3c610f7cf
Lapis0x0 <szy2089931398@gmail.com>
2024-12-12 17:07:01 +0000

    添加开往

--- src/config.ts
+++ src/config.ts
@@ -43,8 +43,8 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      name: '开往',
+      url: 'https://www.travellings.cn/go.html',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
