046731293c49539d9003bb7f0b6842417ce3b6e8
Lapis0x0 <szy2089931398@gmail.com>
2025-06-05 16:17:09 +0800

    测试一下新的搜索组件，理论上采用了更优雅、一站式、鲁棒性更强的索引方式

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,8 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import fs from 'fs';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
